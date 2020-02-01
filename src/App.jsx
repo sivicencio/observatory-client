3a8fb002980d13d4ce1114b9acaacb62af2d6dda
Sebastián Vicencio <sivicencio@gmail.com>
2020-02-01 14:21:07 -0300

    chore(config): add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { Typography } from 'antd';
 
 const { Title } = Typography;
 
-function App() {
+export default function App() {
   return (
-    <Title>Observatory Client</Title>
+    <div>
+      <header>
+        <Title>Observatory Client</Title>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/section">Section</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/section">
+          <Section />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Section() {
+  return <h2>First section</h2>;
+}
